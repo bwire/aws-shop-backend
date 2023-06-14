162418adfc6b0e644ece81a0e13fc688ec9ca83d
barbedwire <e.barbedwire@gmail.com>
2023-06-14 20:30:25 +0200

    install importProductList draft

---
+++ import-service/src/service/import-service.ts
@@ -0,0 +1,5 @@
+export class ImportService {
+  async importProductsFile(): Promise<string> {
+    return 'http://fucking-url.com';
+  };
+}
---
+++ import-service/src/service/index.ts
@@ -0,0 +1,5 @@
+import { ImportService } from './import-service';
+
+export {
+  ImportService,
+}
