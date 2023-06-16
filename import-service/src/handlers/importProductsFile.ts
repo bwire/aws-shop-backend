897f61ff1afe857765d3288d4f319b123888ad24
barbedwire <e.barbedwire@gmail.com>
2023-06-16 08:56:26 +0200

    finish with importProductsFile

--- import-service/src/handlers/importProductsFile.ts
+++ import-service/src/handlers/importProductsFile.ts
@@ -1,12 +1,19 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { ImportService } from "../service/import-service";
 import { successResponse, errorResponse } from './utils';
+import { StatusCodes } from "http-status-codes";
 
 export const importCSVFile = (importService: ImportService) => 
   async (event?: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
       console.log('Incoming request', event);
-      const url = await importService.importProductsFile();
+
+      const name = event?.queryStringParameters?.name;
+      if (!name) {
+        return errorResponse(new Error("Bad request. Parameter 'name' has not been provided"), StatusCodes.BAD_REQUEST);
+      }
+
+      const url = await importService.importProductsFile(name);
       return successResponse(url);
     } 
     catch (err: any) {
--- import-service/src/service/import-service.ts
+++ import-service/src/service/import-service.ts
@@ -1,5 +1,11 @@
+import { S3 } from 'aws-sdk'
 export class ImportService {
-  async importProductsFile(): Promise<string> {
-    return 'http://fucking-url.com';
+  async importProductsFile(name: string): Promise<string> {
+    const s3 = new S3({signatureVersion: 'v4'});
+
+    return s3.getSignedUrl('putObject', {
+      Bucket: process.env.BUCKET_NAME,
+      Key: `uploaded/${name}`,
+    });
   };
 }
