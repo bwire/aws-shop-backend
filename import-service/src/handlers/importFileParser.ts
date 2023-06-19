7543f7f50fc9f73272a9060759cfea19a5cae58e
barbedwire <e.barbedwire@gmail.com>
2023-06-19 17:42:57 +0200

    simplify importFileParser handler

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -1,21 +1,13 @@
-import { APIGatewayProxyResult, S3Event, S3EventRecord } from "aws-lambda";
+import { S3Event, S3EventRecord, S3Handler } from "aws-lambda";
 import { ImportService } from "../service/import-service";
-import { successResponse, errorResponse } from './utils';
 
-export const importParser = (importService: ImportService) => 
-  async (event: S3Event): Promise<APIGatewayProxyResult> => {
-    try {
-      console.log('Incoming request', event);
-      
-      const record: S3EventRecord = event.Records[0];
-      await importService.processCSV(
-        record.s3.bucket.name, 
-        record.s3.object.key
-      );
-
-      return successResponse("Success");
-    } 
-    catch (err: any) {
-      return errorResponse(err);
-    }
+export const importParser = (importService: ImportService): S3Handler => 
+  async (event: S3Event): Promise<void> => {
+    console.log('Incoming request', event);
+    
+    const record: S3EventRecord = event.Records[0];
+    await importService.processCSV(
+      record.s3.bucket.name, 
+      record.s3.object.key
+    ); 
   }
