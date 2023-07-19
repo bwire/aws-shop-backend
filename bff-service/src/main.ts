3165ad5a619120e0488b6242941bced8794fd370
barbedwire <e.barbedwire@gmail.com>
2023-07-19 12:19:29 +0200

    add import service and cart-service

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import { Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,24 @@ async function bootstrap() {
       target: process.env.PRODUCTS_SERVICE,
       changeOrigin: true,
       pathRewrite: { '/api/products': 'products' },
+    }),
+  );
+
+  app.use(
+    '/api/import',
+    createProxyMiddleware((_, req: Request) => req.method === 'GET', {
+      target: process.env.IMPORT_SERVICE,
+      changeOrigin: true,
+      pathRewrite: { '/api/import': 'import' },
+    }),
+  );
+
+  app.use(
+    '/api/cart**',
+    createProxyMiddleware({
+      target: process.env.CART_SERVICE,
+      changeOrigin: true,
+      pathRewrite: { '/api/cart': 'api/profile/cart' },
     }),
   );
 
