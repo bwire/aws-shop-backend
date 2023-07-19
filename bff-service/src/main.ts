fcf2a7d2854badb605eea4cc7dadcc2ad57850d4
barbedwire <e.barbedwire@gmail.com>
2023-07-19 11:41:07 +0200

    add product-service middleware

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    '/api/products**',
+    createProxyMiddleware({
+      target: process.env.PRODUCTS_SERVICE,
+      changeOrigin: true,
+      pathRewrite: { '/api/products': 'products' },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
